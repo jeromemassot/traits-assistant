c935a2aed2ab4a60de920193e0188639230cb920
Jerome MASSOT <j_massot@yahoo.fr>
2025-10-26 13:32:26 -0700

    update icons

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,20 +1,24 @@
 
 import React from 'react';
 
-export const EOLIcon: React.FC = () => (
-    <svg width="60" height="60" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="50" cy="50" r="48" stroke="#0072ce" strokeWidth="4"/>
-        <path d="M25 50C25 36.1929 36.1929 25 50 25C63.8071 25 75 36.1929 75 50" stroke="#0072ce" strokeWidth="4" strokeLinecap="round"/>
-        <path d="M50 25C50 38.8071 38.8071 50 25 50" stroke="#0072ce" strokeWidth="4" strokeLinecap="round"/>
-    </svg>
+import deepmindLogo from '../assets/deepmind-icon.png';
+import smithsonianLogo from '../assets/smithsonian-icon.png';
+
+// 2. Change the components to return <img> elements
+export const DeepmindIcon: React.FC = () => (
+    <img 
+        src={deepmindLogo} 
+        alt="Deepmind logo" 
+        width="60" 
+        height="60" 
+    />
 );
 
 export const SmithsonianIcon: React.FC = () => (
-    <svg width="60" height="60" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="50" cy="50" r="48" fill="#fbb03b"/>
-        <path d="M50 15L65 35L50 55L35 35L50 15Z" fill="#fff"/>
-        <path d="M50 45L65 65L50 85L35 65L50 45Z" fill="#fff"/>
-        <path d="M15 50L35 65L55 50L35 35L15 50Z" fill="#fff"/>
-        <path d="M45 50L65 35L85 50L65 65L45 50Z" fill="#fff"/>
-    </svg>
+    <img 
+        src={smithsonianLogo} 
+        alt="Smithsonian logo" 
+        width="60" 
+        height="60" 
+    />
 );
