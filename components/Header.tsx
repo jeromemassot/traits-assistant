5ad9852691b1686cb94f5f636452145c35a66c5f
Jerome MASSOT <j_massot@yahoo.fr>
2025-10-26 13:33:20 -0700

    change the label for the header button

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ export const Header: React.FC = () => {
                 <div className="flex justify-end items-center h-16">
                     <div className="flex items-center space-x-4">
                         <button className="px-4 py-2 text-sm font-semibold text-white bg-slate-800 rounded-md hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500">
-                            Deploy
+                            Login with Google
                         </button>
                         <button className="p-2 text-slate-500 rounded-full hover:bg-slate-100 hover:text-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500">
                             <DotsVerticalIcon />
